3185bd2be5d6a66b1c813c540e44e1c08ca4ae9c
aditya2337 <aditya.n@gai.co.in>
2017-03-05 16:54:37 +0530

    addded filtree component

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { Card, CardMedia, CardTitle } from 'material-ui/Card';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../../public/css/Tutorial.css';
 
 export default class Tutorial extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      fileDirectory: null
+    };
+  }
+
+  componentWillMount () {
+    console.log(this.props);
+    axios.get(`http://138.197.29.193:3001/users/app/view`)
+      .then(res => console.log(res))
+    ;
+  }
+
   render () {
     const { title, fullObject } = this.props;
     return (
@@ -11,7 +27,7 @@ export default class Tutorial extends Component {
         pathname: '/view-tutorial',
         hash: title,
         state: fullObject
-      }}><Card>
+      }}><Card  >
         <CardTitle title={title} />
       </Card></Link>
     );
