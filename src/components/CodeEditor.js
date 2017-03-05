0df1a2de8a6670e5ac2b18626f85c3d4eb39b311
aditya2337 <aditya.n@gai.co.in>
2017-03-05 19:55:54 +0530

    Trying to write file back to server

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -31,10 +31,8 @@ export default class CodeEditor extends Component {
 
   handleCode (code) {
     const { selectedFile } = this.props.location.state;
-    code = code.toString();
+    code = {code};
     var data = new FormData();
-    console.log(code.toString());
-    data.append('code', code);
     axios.post(`http://138.197.29.193:3002/users/app/save-code?updatedCode=${code}&filePath=${selectedFile}`, data)
       .then(res => this.setState({redirectToRefferer: true}))
       .catch(err => console.error(err))
